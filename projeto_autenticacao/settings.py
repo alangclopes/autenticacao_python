a9a408bd5c1563ace31d740f29ebdf565539aa0d
alangclopes <145778088+alangclopes@users.noreply.github.com>
2024-11-22 10:37:18 -0300

    correcao de estilizacao

--- projeto_autenticacao/settings.py
+++ projeto_autenticacao/settings.py
@@ -105,12 +105,11 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static",
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
